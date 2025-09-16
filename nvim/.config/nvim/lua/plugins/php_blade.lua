0b4688dfec12e24a1e31524dc5706daecb9a5b2e
sonixdev131 <nguyenleson.dev@gmail.com>
2025-09-16 16:14:24 +0700

    abc

--- nvim/.config/nvim/lua/plugins/php_blade.lua
+++ nvim/.config/nvim/lua/plugins/php_blade.lua
@@ -107,7 +107,7 @@ return {
     optional = true,
     opts = {
       formatters_by_ft = {
-        php = { "pint", "php_cs_fixer", stop_after_first = true },
+        php = { "php_cs_fixer" },
         blade = { "blade-formatter" },
       },
       formatters = {
